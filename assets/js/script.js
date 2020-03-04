b98476285c623c8890ad8345ac051e43b320e2ce
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-03-03 18:30:49 -0800

    Added diver, transitions not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
 
 const buttonTop = document.getElementById("toTop");
+
+const diver = document.getElementById('diver');
 const sub = document.getElementById('sub');
-// const line = document.getElementById('line');
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {
     scrollFunction()
+    showSub()
 };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     buttonTop.style.display = "block";
-    // line.style.display = "block";
   } else {
     buttonTop.style.display = "none";
-    // line.style.display = "none";
   }
 }
 
@@ -24,3 +24,17 @@ buttonTop.addEventListener('click', () => window.scrollTo({
   top: 0,
   behavior: 'smooth',
 }));
+
+// When scrolls to #c, show sub
+function showSub() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    // sub.style.display = "block";
+    // sub.classList.add('fade');
+    sub.classList.add('is-shown');
+    // diver.style.display= "none";
+    diver.classList.add('fade');
+    diver.classList.remove('is-shown')
+  } else {
+    // sub.style.display = "none";
+  }
+}
